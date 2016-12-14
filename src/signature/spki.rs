94077356429c1d8948c11366158a80b9321088e4
Marshall Pierce <marshall@mpierce.org>
2016-12-14 09:47:44 -0800

    Convert signed data tests to only test the underlying signature verification (not signature or digest algorithm selection, etc). Inapplicable tests deleted.
    I agree to license my contributions to each file under the terms given
    at the top of each file I changed.

--- src/signature/spki.rs
+++ src/signature/spki.rs
@@ -25,13 +25,9 @@ pub enum VerifyWithSPKIError {
     /// The signature is invalid for the given public key.
     InvalidSignatureForPublicKey,
 
-    /// The signature's algorithm does not match the algorithm of the public
-    /// key it is being validated for. This may be because the public key
-    /// algorithm's OID isn't recognized (e.g. DSA), or the public key
-    /// algorithm's parameters don't match the supported parameters for that
-    /// algorithm (e.g. ECC keys for unsupported curves), or the public key
-    /// algorithm and the signature algorithm simply don't match (e.g.
-    /// verifying an RSA signature with an ECC public key).
+    /// The SignatureAlgorithm does not match the algorithm of the SPKI.
+    /// A mismatch could be because of the algorithm (RSA vs DSA, etc) or the
+    /// parameters (ECDSA_p256 vs ECDSA_384, etc).
     UnsupportedSignatureAlgorithmForPublicKey,
 
     /// The signature algorithm for a signature is not in the set of supported
@@ -59,57 +55,6 @@ pub struct SignedData<'a> {
     /// certificate or OCSP response. This would be the value of
     /// `DigitallySigned.signature` for TLS 1.2 signed data.
     signature: untrusted::Input<'a>,
-}
-
-/// Parses the concatenation of "tbs||signatureAlgorithm||signature" that
-/// is common in the X.509 certificate and OCSP response syntaxes.
-///
-/// X.509 Certificates (RFC 5280) look like this:
-///
-/// ```ASN.1
-/// Certificate (SEQUENCE) {
-///     tbsCertificate TBSCertificate,
-///     signatureAlgorithm AlgorithmIdentifier,
-///     signatureValue BIT STRING
-/// }
-///
-/// OCSP responses (RFC 6960) look like this:
-///
-/// ```ASN.1
-/// BasicOCSPResponse {
-///     tbsResponseData ResponseData,
-///     signatureAlgorithm AlgorithmIdentifier,
-///     signature BIT STRING,
-///     certs [0] EXPLICIT SEQUENCE OF Certificate OPTIONAL
-/// }
-/// ```
-///
-/// Note that this function does NOT parse the outermost `SEQUENCE` or the
-/// `certs` value.
-///
-/// The return value's first component is the contents of
-/// `tbsCertificate`/`tbsResponseData`; the second component is a `SignedData`
-/// structure that can be passed to `verify_signed_data`.
-pub fn parse_signed_data<'a>(der: &mut untrusted::Reader<'a>)
-                             -> Result<(untrusted::Input<'a>, SignedData<'a>),
-                                 VerifyWithSPKIError> {
-    let mark1 = der.mark();
-    let tbs = try!(der::expect_tag_and_get_value(der, der::Tag::Sequence)
-        .map_err(|_| VerifyWithSPKIError::BadDER));
-    let mark2 = der.mark();
-    let data = der.get_input_between_marks(mark1, mark2).unwrap();
-    let algorithm = try!(der::expect_tag_and_get_value(der,
-                                                       der::Tag::Sequence)
-        .map_err(|_| VerifyWithSPKIError::BadDER));
-    let signature = try!(der::bit_string_with_no_unused_bits(der)
-        .map_err(|_| VerifyWithSPKIError::BadDER));
-
-    Ok((tbs,
-        SignedData {
-            data: data,
-            algorithm: algorithm,
-            signature: signature
-        }))
 }
 
 /// Verify `signed_data` using the public key in the DER-encoded
@@ -428,6 +373,38 @@ mod tests {
             &signed_data));
     }
 
+    macro_rules! test_verify_signature {
+        ($fn_name:ident, $file_name:expr, $signature_alg:expr, $expected_result:expr) => {
+            #[test]
+            fn $fn_name() {
+                test_verify_signature($file_name, $signature_alg, $expected_result);
+            }
+        }
+    }
+
+    fn test_verify_signature(file_name: &str,
+                             signature_algorithm: &'static spki::SignatureAlgorithm,
+                             expected_result: Result<(), VerifyWithSPKIError>) {
+        let tsd = parse_test_signed_data(file_name);
+        let spki_value = untrusted::Input::from(&tsd.spki);
+        let spki_value = spki_value.read_all(VerifyWithSPKIError::BadDER, |input| {
+            der::expect_tag_and_get_value(input, der::Tag::Sequence)
+                .map_err(|_| VerifyWithSPKIError::BadDER)
+        }).unwrap();
+
+        let signature = untrusted::Input::from(&tsd.signature);
+        let signature = signature.read_all(VerifyWithSPKIError::BadDER, |input| {
+            der::bit_string_with_no_unused_bits(input)
+                .map_err(|_| VerifyWithSPKIError::BadDER)
+        }).unwrap();
+
+        assert_eq!(expected_result,
+        spki::verify_signature(signature_algorithm,
+                               spki_value,
+                               untrusted::Input::from(&tsd.data),
+                               signature));
+    }
+
     // XXX: This is testing code that isn't even in this module.
     macro_rules! test_verify_signed_data_signature_outer {
         ($fn_name:ident, $file_name:expr, $expected_result:expr) => {
@@ -472,154 +449,139 @@ mod tests {
 
     // XXX: Some of the BadDER tests should have better error codes, maybe?
 
+    // TODO need ECDSA_P256_SHA512 for these tests
     // XXX: We should have a variant of this test with a SHA-256 digest that gives
     // `VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey`.
-    test_verify_signed_data!(
-        test_ecdsa_prime256v1_sha512_spki_params_null,
-        "ecdsa-prime256v1-sha512-spki-params-null.pem",
-        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    test_verify_signed_data_signature_outer!(
-        test_ecdsa_prime256v1_sha512_unused_bits_signature,
-        "ecdsa-prime256v1-sha512-unused-bits-signature.pem",
-        VerifyWithSPKIError::BadDER);
+//    test_verify_signature!(
+//        test_ecdsa_prime256v1_sha512_spki_params_null,
+//        "ecdsa-prime256v1-sha512-spki-params-null.pem",
+//        &spki::ECDSA_P256_SHA512,
+//        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
+//    test_verify_signature!(
+//        test_ecdsa_prime256v1_sha512_using_ecdh_key,
+//        "ecdsa-prime256v1-sha512-using-ecdh-key.pem",
+//        &spki::ECDSA_P256_SHA512,
+//        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
     // XXX: We should have a variant of this test with a SHA-256 digest that gives
     // `VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey`.
-    test_verify_signed_data!(
-        test_ecdsa_prime256v1_sha512_using_ecdh_key,
-        "ecdsa-prime256v1-sha512-using-ecdh-key.pem",
-        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    // XXX: We should have a variant of this test with a SHA-256 digest that gives
-    // `VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey`.
-    test_verify_signed_data!(
-        test_ecdsa_prime256v1_sha512_using_ecmqv_key,
-        "ecdsa-prime256v1-sha512-using-ecmqv-key.pem",
-        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    test_verify_signed_data!(
-        test_ecdsa_prime256v1_sha512_using_rsa_algorithm,
-        "ecdsa-prime256v1-sha512-using-rsa-algorithm.pem",
-        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
+//    test_verify_signature!(
+//        test_ecdsa_prime256v1_sha512_using_ecmqv_key,
+//        "ecdsa-prime256v1-sha512-using-ecmqv-key.pem",
+//        &spki::ECDSA_P256_SHA512,
+//        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
     // XXX: We should have a variant of this test with a SHA-256 digest that gives
     // `VerifyWithSPKIError::InvalidSignatureForPublicKey`.
-    test_verify_signed_data!(
-        test_ecdsa_prime256v1_sha512_wrong_signature_format,
-        "ecdsa-prime256v1-sha512-wrong-signature-format.pem",
-        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
+//    test_verify_signature!(
+//        test_ecdsa_prime256v1_sha512_wrong_signature_format,
+//        "ecdsa-prime256v1-sha512-wrong-signature-format.pem",
+//        &spki::ECDSA_P256_SHA512,
+//        Err(VerifyWithSPKIError::InvalidSignatureForPublicKey));
     // Differs from Chromium because we don't support P-256 with SHA-512.
-    test_verify_signed_data!(
-        test_ecdsa_prime256v1_sha512,
-        "ecdsa-prime256v1-sha512.pem",
-        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    test_verify_signed_data!(test_ecdsa_secp384r1_sha256_corrupted_data,
+//    test_verify_signature!(
+//        test_ecdsa_prime256v1_sha512,
+//        "ecdsa-prime256v1-sha512.pem",
+//        &spki::ECDSA_P256_SHA512,
+//        Ok());
+    test_verify_signature!(test_ecdsa_secp384r1_sha256_corrupted_data,
                              "ecdsa-secp384r1-sha256-corrupted-data.pem",
+                             &spki::ECDSA_P384_SHA256,
                              Err(VerifyWithSPKIError::InvalidSignatureForPublicKey));
-    test_verify_signed_data!(test_ecdsa_secp384r1_sha256,
-                             "ecdsa-secp384r1-sha256.pem", Ok(()));
-    test_verify_signed_data!(
+    test_verify_signature!(test_ecdsa_secp384r1_sha256,
+                             "ecdsa-secp384r1-sha256.pem",
+                              &spki::ECDSA_P384_SHA256,
+                              Ok(()));
+    test_verify_signature!(
         test_ecdsa_using_rsa_key, "ecdsa-using-rsa-key.pem",
+        &spki::ECDSA_P256_SHA256,
         Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
 
+    // TODO this only tests DER decoding, not signature logic
     test_parse_spki_bad_outer!(test_rsa_pkcs1_sha1_bad_key_der_length,
                                "rsa-pkcs1-sha1-bad-key-der-length.pem",
                                VerifyWithSPKIError::BadDER);
     test_parse_spki_bad_outer!(test_rsa_pkcs1_sha1_bad_key_der_null,
                                "rsa-pkcs1-sha1-bad-key-der-null.pem",
                                VerifyWithSPKIError::BadDER);
-    test_verify_signed_data!(test_rsa_pkcs1_sha1_key_params_absent,
+    test_verify_signature!(test_rsa_pkcs1_sha1_key_params_absent,
                              "rsa-pkcs1-sha1-key-params-absent.pem",
+                             &spki::RSA_PKCS1_2048_8192_SHA1,
                              Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
-    test_verify_signed_data!(
-        test_rsa_pkcs1_sha1_using_pss_key_no_params,
+    // We only support rsa keys identified as "rsaEncyrption", not rsa pss, so this is really only
+    // a test that "rsaEncryption" != "rsassapss", not about params.
+    test_verify_signature!( test_rsa_pkcs1_sha1_using_pss_key_no_params,
         "rsa-pkcs1-sha1-using-pss-key-no-params.pem",
+        &spki::RSA_PKCS1_2048_8192_SHA1,
         Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
-    test_verify_signed_data!(test_rsa_pkcs1_sha1_wrong_algorithm,
-                             "rsa-pkcs1-sha1-wrong-algorithm.pem",
-                             Err(VerifyWithSPKIError::InvalidSignatureForPublicKey));
     // XXX: RSA PKCS#1 with SHA-1 is a supported algorithm, but we only accept
     // 2048-8192 bit keys, and this test file is using a 1024 bit key. Thus,
     // our results differ from Chromium's. TODO: this means we need a 2048+ bit
     // version of this test.
-    test_verify_signed_data!(test_rsa_pkcs1_sha1, "rsa-pkcs1-sha1.pem",
-                             Err(VerifyWithSPKIError::InvalidSignatureForPublicKey));
+    test_verify_signature!(test_rsa_pkcs1_sha1,
+                           "rsa-pkcs1-sha1.pem",
+                           &spki::RSA_PKCS1_2048_8192_SHA1,
+                           Err(VerifyWithSPKIError::InvalidSignatureForPublicKey));
     // XXX: RSA PKCS#1 with SHA-1 is a supported algorithm, but we only accept
     // 2048-8192 bit keys, and this test file is using a 1024 bit key. Thus,
     // our results differ from Chromium's. TODO: this means we need a 2048+ bit
     // version of this test.
-    test_verify_signed_data!(test_rsa_pkcs1_sha256, "rsa-pkcs1-sha256.pem",
-                             Err(VerifyWithSPKIError::InvalidSignatureForPublicKey));
+    test_verify_signature!(test_rsa_pkcs1_sha256,
+                           "rsa-pkcs1-sha256.pem",
+                           &spki::RSA_PKCS1_2048_8192_SHA256,
+                           Err(VerifyWithSPKIError::InvalidSignatureForPublicKey));
+    // TODO this only tests DER decoding, not signature logic
     test_parse_spki_bad_outer!(test_rsa_pkcs1_sha256_key_encoded_ber,
                                "rsa-pkcs1-sha256-key-encoded-ber.pem",
                                VerifyWithSPKIError::BadDER);
-    test_verify_signed_data!(test_rsa_pkcs1_sha256_spki_non_null_params,
+    test_verify_signature!(test_rsa_pkcs1_sha256_spki_non_null_params,
                              "rsa-pkcs1-sha256-spki-non-null-params.pem",
+                             &spki::RSA_PKCS1_2048_8192_SHA256,
                              Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
-    test_verify_signed_data!(
-        test_rsa_pkcs1_sha256_using_ecdsa_algorithm,
-        "rsa-pkcs1-sha256-using-ecdsa-algorithm.pem",
-        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
-    test_verify_signed_data!(
+    test_verify_signature!(
         test_rsa_pkcs1_sha256_using_id_ea_rsa,
         "rsa-pkcs1-sha256-using-id-ea-rsa.pem",
+        &spki::RSA_PKCS1_2048_8192_SHA256,
         Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
-
-    // Chromium's PSS test are for parameter combinations we don't support.
-    test_verify_signed_data!(test_rsa_pss_sha1_salt20_using_pss_key_no_params,
-                             "rsa-pss-sha1-salt20-using-pss-key-no-params.pem",
-                             Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    test_verify_signed_data!(
-        test_rsa_pss_sha1_salt20_using_pss_key_with_null_params,
-        "rsa-pss-sha1-salt20-using-pss-key-with-null-params.pem",
-        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    test_verify_signed_data!(test_rsa_pss_sha1_salt20, "rsa-pss-sha1-salt20.pem",
-                             Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    test_verify_signed_data!(test_rsa_pss_sha1_wrong_salt,
-                             "rsa-pss-sha1-wrong-salt.pem",
-                             Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    test_verify_signed_data!(test_rsa_pss_sha256_mgf1_sha512_salt33,
-                             "rsa-pss-sha256-mgf1-sha512-salt33.pem",
-                             Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    test_verify_signed_data!(
-        test_rsa_pss_sha256_salt10_using_pss_key_with_params,
-        "rsa-pss-sha256-salt10-using-pss-key-with-params.pem",
-        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    test_verify_signed_data!(
-        test_rsa_pss_sha256_salt10_using_pss_key_with_wrong_params,
-        "rsa-pss-sha256-salt10-using-pss-key-with-wrong-params.pem",
-        Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
-    test_verify_signed_data!(test_rsa_pss_sha256_salt10,
-                             "rsa-pss-sha256-salt10.pem",
-                             Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithm));
 
     /// Our PSS tests that should work.
-    test_verify_signed_data!(
+    test_verify_signature!(
         test_rsa_pss_sha256_salt32,
         "ours/rsa-pss-sha256-salt32.pem",
+        &spki::RSA_PSS_2048_8192_SHA256_LEGACY_KEY,
         Ok(()));
-    test_verify_signed_data!(
+    test_verify_signature!(
         test_rsa_pss_sha384_salt48,
         "ours/rsa-pss-sha384-salt48.pem",
+        &spki::RSA_PSS_2048_8192_SHA384_LEGACY_KEY,
         Ok(()));
-    test_verify_signed_data!(
+    test_verify_signature!(
         test_rsa_pss_sha512_salt64,
         "ours/rsa-pss-sha512-salt64.pem",
+        &spki::RSA_PSS_2048_8192_SHA512_LEGACY_KEY,
         Ok(()));
-    test_verify_signed_data!(
+    test_verify_signature!(
         test_rsa_pss_sha256_salt32_corrupted_data,
         "ours/rsa-pss-sha256-salt32-corrupted-data.pem",
+        &spki::RSA_PSS_2048_8192_SHA256_LEGACY_KEY,
         Err(VerifyWithSPKIError::InvalidSignatureForPublicKey));
-    test_verify_signed_data!(
+    test_verify_signature!(
         test_rsa_pss_sha384_salt48_corrupted_data,
         "ours/rsa-pss-sha384-salt48-corrupted-data.pem",
+        &spki::RSA_PSS_2048_8192_SHA384_LEGACY_KEY,
         Err(VerifyWithSPKIError::InvalidSignatureForPublicKey));
-    test_verify_signed_data!(
+    test_verify_signature!(
         test_rsa_pss_sha512_salt64_corrupted_data,
         "ours/rsa-pss-sha512-salt64-corrupted-data.pem",
+        &spki::RSA_PSS_2048_8192_SHA512_LEGACY_KEY,
         Err(VerifyWithSPKIError::InvalidSignatureForPublicKey));
 
-    test_verify_signed_data!(
+    test_verify_signature!(
         test_rsa_using_ec_key, "rsa-using-ec-key.pem",
+        &spki::RSA_PKCS1_2048_8192_SHA256,
         Err(VerifyWithSPKIError::UnsupportedSignatureAlgorithmForPublicKey));
-    test_verify_signed_data!(test_rsa2048_pkcs1_sha512,
-                             "rsa2048-pkcs1-sha512.pem", Ok(()));
+    test_verify_signature!(test_rsa2048_pkcs1_sha512,
+                           "rsa2048-pkcs1-sha512.pem",
+                           &spki::RSA_PKCS1_2048_8192_SHA512,
+                           Ok(()));
 
     struct TestSignedData {
         spki: std::vec::Vec<u8>,
